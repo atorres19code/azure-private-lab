b44ec386d8a8ba85fa4a512c4484a4e512fe9395
atorres19code <54910179+atorres19code@users.noreply.github.com>
2025-09-25 11:06:27 -0600

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,1 @@
-output "app_service_url" {
-  description = "URL para acceder a la aplicación web (solo accesible desde el VNet)."
-  value       = azurerm_app_service.app_service.default_site_hostname
-}
 
-output "private_ip" {
-  description = "La IP privada del Private Endpoint."
-  value       = azurerm_private_endpoint.private_endpoint.private_service_connection[0].private_ip_address
-}
