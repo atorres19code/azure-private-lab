ce978ec2d38a8c8f6df4e5c421694a37ba1d504a
atorres19code <54910179+atorres19code@users.noreply.github.com>
2025-09-25 09:33:28 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,12 +41,12 @@ resource "azurerm_app_service_plan" "asp" {
 }
 
 resource "azurerm_app_service" "app_service" {
-  name                = "private-webapp-${random_string.random.result}" # Nombre único
+  name                = "private-webapp-${random_string.random.result}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.asp.id
   site_config {
-    linux_fx_version = "NODE|18-lts" # Puedes cambiar esto por la pila que necesites (ej. "DOTNET|6.0", "PYTHON|3.9")
+    linux_fx_version = "NODE|18-lts"
   }
   https_only = true
 }
@@ -93,6 +93,3 @@ resource "random_string" "random" {
   upper   = false
   special = false
 }
-
-```eof
----
