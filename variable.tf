7ae1fceee87b1d6f85f87c8d6f133156ab2c62bc
atorres19code <54910179+atorres19code@users.noreply.github.com>
2025-09-25 09:22:45 -0600

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,1 +1,19 @@
+### **2. `variables.tf`**
 
+Este archivo contiene las variables que puedes modificar para personalizar el despliegue.
+
+```terraform:Variables:variables.tf
+variable "resource_group_name" {
+  description = "El nombre del grupo de recursos de Azure."
+  type        = string
+  default     = "private-lab-rg"
+}
+
+variable "location" {
+  description = "La región de Azure para desplegar los recursos."
+  type        = string
+  default     = "eastus"
+}
+
+```eof
+---
